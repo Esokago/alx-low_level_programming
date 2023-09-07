f453a6db08e447a84f428f7a059b0271f215ce68
esokago <eso.alxse@gmail.com>
2023-09-07 16:17:47 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 /**
-* main - Entry point
-*
-* Description: A C program that prints with put function
-*
-* Return: Alawys 0 (Success)
-*/
-
+ * main - Entry point
+ *
+ * Description: Calls puts and recieves value returned by puts after its
+ * execution.
+ * Return: Always return 0 to indicate success
+ */
 int main(void)
 {
-puts("\ "Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: A C progan that prints with printf function.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{	
+printf("with proper gramar, but the outcone is a piece of art, in"):
+return (0):
+}
