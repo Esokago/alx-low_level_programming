be2be56d1c123c03e427a5322f3191f51968a441
esokago <eso.alxse@gmail.com>
2023-09-07 17:09:20 -0700

    lolo

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,13 @@
 /**
 *main - Entry point
 *
-*Description: A C progam that prints with print function.
+*Description: A C program that print function
 *
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-printf("with proper gramar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
