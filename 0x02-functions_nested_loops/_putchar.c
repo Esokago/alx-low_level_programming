ce6db6de162a0120c436e907655dcc702338bd17
esokago <eso.alxse@gmail.com>
2023-09-12 13:35:59 -0700

    yes

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,7 +3,7 @@
 
 /**
 *_putchar - write the charachter c to stdout
-*0c: the character to print
+*@c: the character to print
 *
 *Return: on success 1
 *        on error, -1 is returned, and error 0 is set appropritely.
