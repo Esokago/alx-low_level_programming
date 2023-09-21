3af48b701c583a04244fa9f51f4f4915898ab481
esokago <eso.alxse@gmail.com>
2023-09-21 13:16:35 -0700

    9

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,17 +2,18 @@
 /**
 *cap_string - function that capitalize first character of a word
 *@str: string to capitalize
+*
 *Return:returns the capitalized string
 */
+
 char *cap_string(char *str)
 {
-	int index = 0;
+	int i;
+       	i = 0;
 
-	while (str[++index])
+	while (str[i] != '\0')
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
+	
 		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
