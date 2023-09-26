4d2a666169d745a4232dd1ee178f7e8dd37958f1
esokago <eso.alxse@gmail.com>
2023-09-26 06:08:23 -0700

    o

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*print_diagsums -  a function that prints sum
+*@a:matrix to sum
+*@size:matrix size
+*/
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		a += size;
+	}
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
