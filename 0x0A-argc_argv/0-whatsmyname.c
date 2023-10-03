e5f517e93419bda2b9e5d963ae41f494c16806da
esokago <MY_NAME@example.com>
2023-10-03 05:39:27 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main -  program name
+*@argc: num of arguments
+*@argv: arguments.
+*Return: 0
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	(void)agrv;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
