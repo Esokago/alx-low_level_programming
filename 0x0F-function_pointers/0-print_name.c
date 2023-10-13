cd1be9bbd0e6bf140f8ad5e86e0fa4f1d56c6bae
esokago <eso.alxse@gmail.com>
2023-10-13 08:36:25 -0700

    yes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,18 @@
 
 /**
  * print_name - prints a name as is
- * @name: the person name
- * @f: this is the pointer
+ * @name: name of the person
+ * @f: is pointer
 **/
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+	{
 		f(name);
+	}
 }
